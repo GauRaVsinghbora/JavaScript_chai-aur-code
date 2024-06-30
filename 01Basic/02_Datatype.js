76df427802e401fcc9317454924da23ebd643fe1
GauRaV <139610787+GauRaVsinghbora@users.noreply.github.com>
2024-06-30 05:44:00 +0000

    datatypes

--- 01Basic/02_Datatype.js
+++ 01Basic/02_Datatype.js
@@ -1,3 +1,24 @@
 "Use Strict"   /* treat all Js code as newer version */
 
-alert(3 + 3);
+// alert(3+3) /* we are using the nodejs, not brower
+
+console.log(3+3); 
+
+let name="Gaurav";
+let age= 19;
+
+
+/*premitive datatype*/
+
+// number =>2 to power 53
+// bigint
+// string => ""
+// boolean => True/false
+// null  => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof undefined) // type undefined.
+console.log(typeof null) // type object.
