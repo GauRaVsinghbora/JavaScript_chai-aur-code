a7b54a027eebf656f75e2a718148e83223f5fe82
GauRaV <139610787+GauRaVsinghbora@users.noreply.github.com>
2024-07-03 04:14:30 +0000

    datatype summary

---
+++ 01_Basic/datatypes_summary.js
@@ -0,0 +1,25 @@
+/* =============================== Primitive ====================== */
+
+
+//while calling the variable,the copy come made it is know as call by value.
+//7      Number, boolean, string, null , undefined, BigInt,symbol.
+
+
+/* =============================== reference ====================== */
+
+//while calling the variable the actual memory location is come and it is know as call by reference.
+// array , object ,function.
+
+
+const heros = ["ironman","captial america","thor"];
+
+let Myobj ={
+    name:"Gaurav",
+    age:"19",
+}
+const myfunction= function(){
+    console.log("hello! world");
+}
+
+console.log(typeof BigInt);
+console.log("gaurav");
